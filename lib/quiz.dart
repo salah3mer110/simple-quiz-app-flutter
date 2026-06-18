fea3459e37ed478fd86d092b91f2e2bdbeb48e78
salah3mer110 <salahamer3040@gmail.com>
2026-06-18 12:01:10 +0300

    Initial commit

---
+++ lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:simple_quiz_app/questions_screen.dart';
+import 'package:simple_quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchActiveScreens() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 78, 13, 151),
+                const Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchActiveScreens)
+              : QuestionsScreen(),
+        ),
+      ),
+    );
+  }
+}
